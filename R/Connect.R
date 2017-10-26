b19cdb13e0f96d521b12a8e82538697b6909c789
Alex Harden <alex.harden@evidera.com>
2017-10-26 14:52:38 -0400

    Update Redshift driver

--- R/Connect.R
+++ R/Connect.R
@@ -3,13 +3,13 @@
 # Copyright 2017 Observational Health Data Sciences and Informatics
 #
 # This file is part of DatabaseConnector
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -417,7 +417,7 @@ connect <- function(connectionDetails,
   }
   if (dbms == "redshift") {
     writeLines("Connecting using Redshift driver")
-    pathToJar <- system.file("java", "RedshiftJDBC4-1.2.8.1005.jar", package = "DatabaseConnector")
+    pathToJar <- system.file("java", "RedshiftJDBC4-1.2.10.1009.jar", package = "DatabaseConnector")
     driver <- getJbcDriverSingleton("com.amazon.redshift.jdbc4.Driver", pathToJar)
     if (missing(connectionString) || is.null(connectionString)) {
       if (!grepl("/", server))
